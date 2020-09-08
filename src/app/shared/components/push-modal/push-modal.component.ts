0934df2d79df2be1090803e32c3d4bdeff1ca4d3
Corentin Gautier <corentin.gautier@consertotech.pro>
2020-09-08 14:24:05 +0200

    move components to the shared folder

---
+++ src/app/shared/components/push-modal/push-modal.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+import { CG44Notification } from '../../models/notification';
+
+@Component({
+  selector: 'app-push-modal',
+  templateUrl: './push-modal.component.html',
+  styleUrls: ['./push-modal.component.scss'],
+})
+export class PushModalComponent implements OnInit {
+
+  public notification: CG44Notification;
+  public icon: string;
+
+  constructor(private modalController: ModalController) { }
+
+  ngOnInit() {
+    switch (this.notification.data.type.toLowerCase()) {
+      case 'accident':
+        this.icon = 'accident';
+        break;
+      case 'travaux':
+        this.icon = 'travaux';
+        break;
+      case 'vent':
+        this.icon = 'vent-fort';
+        break;
+      default:
+        this.icon = 'particulier';
+        break;
+    }
+  }
+
+  dismiss() {
+    this.modalController.dismiss();
+  }
+
+}
