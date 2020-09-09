97d3cae2cbb75f448b933e2f7df48155bb961ba9
Corentin Gautier <corentin.gautier@consertotech.pro>
2020-09-09 17:07:35 +0200

    feat(webcam): add webcam page

--- src/app/pages/camera/camera.page.ts
+++ src/app/pages/camera/camera.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiService } from '../../shared/services/api.service';
+import { LiaisonService } from '../../shared/services/liaison.service';
+
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.page.html',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CameraPage implements OnInit {
 
-  constructor() { }
+  public firstWebcam: string;
+  public secondWebcam: string;
+  public date: Date;
+  public currentLiaison: any;
+
+  constructor(private liaisonService: LiaisonService, private apiService: ApiService) { }
 
   ngOnInit() {
+    this.liaisonService.currentDirectionObserver.subscribe(() => {
+      this.getWebcams();
+    });
+  }
+
+  async getWebcams() {
+    this.currentLiaison = this.liaisonService.getCurrentLiaison();
+    const currentDirection = this.liaisonService.getCurrent();
+
+    this.firstWebcam = await this.apiService.getLatestWebcam(currentDirection.data.webcamIds[0]);
+    this.secondWebcam = await this.apiService.getLatestWebcam(currentDirection.data.webcamIds[1]);
+
+    this.date = new Date();
   }
 
 }
