bfa8f8af466dfe6013a543e2a9deeacd4720bf19
Corentin Gautier <corentin.gautier@consertotech.pro>
2020-09-09 14:13:01 +0200

    improve liaisonService doc and DRY

--- src/app/shared/services/liaison.service.ts
+++ src/app/shared/services/liaison.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
-
-import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -29,7 +27,7 @@ export class LiaisonService {
    * Toggle between north and south
    */
   changeDirection() {
-    this.currentDirection = this.currentDirection === 'north' ? 'south' : 'north';
+    this.currentDirection = this.getDirectionName();
     this.currentDirectionObserver.next(this.currentDirection);
   }
 
@@ -42,21 +40,30 @@ export class LiaisonService {
     return environment.liaisons.find(item => item.id === this.currentLiaisonId);
   }
 
-  private getCurrentDirection() {
+  private getCurrentDirectionData() {
     const currentLiaison = this.getCurrentLiaison();
-    return currentLiaison[this.currentDirection === 'north' ? 'south' : 'north'];
+    return currentLiaison[this.getDirectionName()];
+  }
+
+  /**
+   * When selecting the north dock, the direction is south and vice versa
+   */
+  private getDirectionName() {
+    return this.currentDirection === 'north' ? 'south' : 'north';
   }
 
   /**
    * Returns the name of the start point according to the direction
+   * start point = !direction (south when going north and vice versa)
    */
   getStartPoint() {
     const currentLiaison = this.getCurrentLiaison();
-    return currentLiaison[this.currentDirection === 'north' ? 'south' : 'north'].name;
+    return currentLiaison[this.getDirectionName()].name;
   }
 
   /**
    * Returns the name of the end point according to the direction
+   * end point = direction (north when going north)
    */
   getEndPoint() {
     const currentLiaison = this.getCurrentLiaison();
@@ -67,7 +74,7 @@ export class LiaisonService {
     return {
       from: this.getStartPoint(),
       to: this.getEndPoint(),
-      data: this.getCurrentDirection()
+      data: this.getCurrentDirectionData()
     };
   }
 }
