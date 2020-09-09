ae93ef6a4f02361008fdd325e82e42629448f1fc
Corentin Gautier <corentin.gautier@consertotech.pro>
2020-09-09 14:30:36 +0200

    feat(home): add home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 import { LiaisonService } from '../../shared/services/liaison.service';
 
@@ -9,12 +10,16 @@ import { LiaisonService } from '../../shared/services/liaison.service';
 })
 export class HomePage implements OnInit {
 
-  constructor(private liaisonService: LiaisonService) { }
+  constructor(private liaisonService: LiaisonService, private menuController: MenuController) { }
 
   ngOnInit() { }
 
   openLiaison(id: string, direction: string) {
     this.liaisonService.openLiaison(id, direction);
+  }
+
+  toggleMenu() {
+    this.menuController.toggle();
   }
 
 }
